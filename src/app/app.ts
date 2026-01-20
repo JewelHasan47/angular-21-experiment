69fd7d99c2344f039a06df1062802743db230be7
Jewel Hasan <jewelhasan477@gmail.com>
2026-01-20 18:27:23 +0600

    added select box

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { Accordion } from './components/aria/accordion/accordion';
+import { SelectBox } from './components/aria/select-box/select-box';
 
 @Component( {
     selector: 'app-root',
-    imports: [ Accordion ],
+    imports: [
+        Accordion,
+        SelectBox
+    ],
     templateUrl: './app.html',
     styleUrl: './app.css'
 } )
